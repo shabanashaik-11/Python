d0261b16864fb7e35a76445993186dd7d26eba91
shabanashaik-11 <shabanashaikshabana11@gmail.com>
2025-03-12 22:21:46 +0530

    Create Sessions.py

---
+++ Sessions.py
@@ -0,0 +1,9 @@
+month =int(input())
+if month >2 and month<=5:
+   print("summer")
+if month>=6 and month<=9:
+   print("rain")
+if month>=10 and month<=11:
+   print("autum")
+if month>=12 and month<=2:
+   print("winter")
